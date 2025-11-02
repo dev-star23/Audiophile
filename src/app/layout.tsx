e075f50112b7ccd8437af5a7fdf852dfa72e4475
dev-star23 <khadejasuliman703@gmail.com>
2025-11-02 13:01:09 +0300

    chore: layout min height

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,12 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <body
-        className={`${manrope.variable} ${geistMono.variable} antialiased`}
+        className={`${manrope.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
         <Navbar />
-        {children}
+        <div className="flex-1">
+          {children}
+        </div>
         <Footer />
       </body>
     </html>
